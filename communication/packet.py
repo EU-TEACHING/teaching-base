9568744f0e5e18940434fe0e3135231d7badb9d7
vdecaro <valeriodc95@hotmail.it>
2022-04-01 17:14:33 +0200

    list of timestamps allowed

--- communication/packet.py
+++ communication/packet.py
@@ -3,7 +3,7 @@ from dataclasses_json import dataclass_json
 
 import datetime
 import os
-from typing import Dict
+from typing import Dict, List, Union
 
 @dataclass_json
 @dataclass
@@ -12,5 +12,5 @@ class DataPacket:
     service_type: str = field(default_factory=os.environ('SERVICE_TYPE'))
     service_name: str = field(default_factory=os.environ('SERVICE_NAME'))
     topic: str = ''
-    timestamp: datetime.datetime = field(default_factory=datetime.now())
+    timestamp: Union[datetime.datetime, List[datetime.datetime]] = field(default_factory=datetime.now())
     body: Dict = {}
