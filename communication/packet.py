c7f74dede125c733eda96a4276050bbef54a00e8
vdecaro <valeriodc95@hotmail.it>
2022-04-19 16:29:46 +0200

    default fix

--- communication/packet.py
+++ communication/packet.py
@@ -12,5 +12,5 @@ class DataPacket:
     service_type: str = os.environ['SERVICE_TYPE']
     service_name: str = os.environ['SERVICE_NAME']
     topic: str = ''
-    timestamp: Union[datetime.datetime, List[datetime.datetime]] = field(default_factory=datetime.datetime.now())
-    body: Dict = field(default_factory={})
+    timestamp: Union[datetime.datetime, List[datetime.datetime]] = field(default_factory=datetime.datetime.now)
+    body: Dict = field(default_factory=dict)
